6c8a30277fe17df991c6846bfe9d6c6dfce9230c
Damilola Akinlade <damak.dev@gmail.com>
2023-04-15 11:56:09 +0100

    font weight

--- client/components/UI/Logo.tsx
+++ client/components/UI/Logo.tsx
@@ -4,7 +4,7 @@ export default function Logo({ url }: { url?: string }) {
   return (
     <Link href={url || "/"}>
       <div className="flex items-center justify-center h-20 w-20 bg-gradient-to-br from-[#375CA9] to-pink-500 rounded-full shadow-lg">
-        <h1 className="text-2xl text-center font-bold leading-none text-white tracking-tight">
+        <h1 className="text-2xl text-center font-extralight leading-none text-white tracking-tight">
           Travel
           <br />
           Log
